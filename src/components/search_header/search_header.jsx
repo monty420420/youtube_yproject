2d6887f2388b51c751eca4fcbdd710f8c55ba245
Muntaeyeon <monty420420@naver.com>
2022-01-30 00:15:26 +0900

    video_search_header4

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,11 +1,11 @@
 import styles from './search_header.module.css';
 import React from 'react';
 
+
 const SearchHeader = (props) => {
     return (
         <header className={styles.header}>
-            <img className={styles.img} src='/images/logo.png' alt='logo' />
-            <h1 className={styles.title}>Youtube</h1>
+            <img className={styles.img} src='/images/youtube.png' alt='logo' />
             <input className={styles.input} type="search" placeholder='검색'/>
             <button className={styles.btn} type='submit'>
                <img src='/images/search.png' alt='search'/>

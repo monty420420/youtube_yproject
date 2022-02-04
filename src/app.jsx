9746b236423aa6cf99d40eb061b5852ae8ab5114
Muntaeyeon <monty420420@naver.com>
2022-02-04 21:32:42 +0900

    api_refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -4,33 +4,17 @@ import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=AIzaSyA5dDgQGgCSG-5ndYsSHDpRhpMxHRdgYc0`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId}))) //result.items 배열을 빙글 빙글 돌면서 각각의 배열 아이템들의 id값을 새로운 id로 덮어 씌워줌
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube.search(query)
+    .then(console.log);
+
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyA5dDgQGgCSG-5ndYsSHDpRhpMxHRdgYc0",
-     requestOptions
-     )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube.mostPopular()
+    .then(console.log);
   }, []);
 
  
---
+++ src/service/youtube.js
@@ -0,0 +1,28 @@
+class Youtube {
+    constructor(key) {
+        this.key = key;
+        this.getRequestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+    }
+
+    mostPopular() {
+        return fetch("https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyA5dDgQGgCSG-5ndYsSHDpRhpMxHRdgYc0",
+        this.getRequestOptions
+        )
+         .then(response => response.json())
+         .then(result => result.items); //app.jsx에 함수인자를 setVideos라고 만들어 두엇지만 여기 함수인자를 result라고 만들어도 상관없음
+    }
+
+    search(query) {
+        return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=AIzaSyA5dDgQGgCSG-5ndYsSHDpRhpMxHRdgYc0`,
+        this.getRequestOptions
+        )
+         .then(response => response.json())
+         .then(result => result.items.map(item => ({...item, id: item.id.videoId}))) //result.items 배열을 빙글 빙글 돌면서 각각의 배열 아이템들의 id값을 새로운 id로 덮어 씌워줌
+     }
+   
+}
+
+export default Youtube;

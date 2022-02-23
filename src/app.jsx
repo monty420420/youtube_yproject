6314c2d1cac3bc6812aa39a1453f99e0425c2b65
Muntaeyeon <monty420420@naver.com>
2022-02-24 00:31:18 +0900

    memo

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
 
 
-function App({youtube}) {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
@@ -13,18 +13,18 @@ function App({youtube}) {
     setSelectedVideo(video);
   };
   
-  const search = query => {
+  const search = useCallback (query => {  //usecallback 사용하여 item클릭시마다 헤더 재랜더링 안되게하기 //usecallback은 만들어두면 메모리상에 계속 보관
     youtube.search(query) //
     .then(videos => {
       setVideos(videos);
       setSelectedVideo(null);  //setSelectedVideo를 null로하여 기존비디오값보여주기
     });
-  };
+  }, [youtube]);
 
   useEffect(() => {
     youtube.mostPopular() //
     .then(videos => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
  
 
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,37 +1,39 @@
 import styles from './search_header.module.css';
-import React, { useRef } from 'react';
+import React, { memo, useRef } from 'react';
 
 
-const SearchHeader = ({onSearch}) => {
-    const inputRef = useRef();
-    const handleSearch = () => {
-       const value = inputRef.current.value;
-       //console.log(value);
-       onSearch(value);
-    }
-
-    const onClick = () => {
-       handleSearch();
-    };
-
-    const onKeyPress = (event) => {
-       if (event.key === 'Enter') {
+const SearchHeader = memo (
+    ({onSearch}) => {
+        const inputRef = useRef();
+        const handleSearch = () => {
+           const value = inputRef.current.value;
+           //console.log(value);
+           onSearch(value);
+        }
+    
+        const onClick = () => {
            handleSearch();
-       }
-    };
-
-    return (
-        <header className={styles.header}>
-            <img className={styles.img} src='/images/youtube.png' alt='logo' />
-            <input className={styles.input} type="search" 
-                   placeholder='검색'onKeyPress={onKeyPress} 
-                   ref={inputRef}
-            />
-            <button className={styles.btn} type='submit' onClick={onClick}>
-               <img src='/images/search.png' className={styles.sImage} alt='search'/>
-            </button>
-        </header>
-    )
-}
+        };
+    
+        const onKeyPress = (event) => {
+           if (event.key === 'Enter') {
+               handleSearch();
+           }
+        };
+         
+        return (
+            <header className={styles.header}>
+                <img className={styles.img} src='/images/youtube.png' alt='logo' />
+                <input className={styles.input} type="search" 
+                       placeholder='검색'onKeyPress={onKeyPress} 
+                       ref={inputRef}
+                />
+                <button className={styles.btn} type='submit' onClick={onClick}>
+                   <img src='/images/search.png' className={styles.sImage} alt='search'/>
+                </button>
+            </header>
+        )
+    }
+);
 
 export default SearchHeader;

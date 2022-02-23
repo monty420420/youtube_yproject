49d21787aa4915df10f9ff578fe95525fb7ad700
Muntaeyeon <monty420420@naver.com>
2022-02-24 01:11:32 +0900

    axios

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,26 +1,34 @@
+import axios from "axios";
+
 class Youtube {
     constructor(key) {
-        this.key = key;
-        this.getRequestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
+        this.youtube = axios.create({
+          baseURL: 'https://youtube.googleapis.com/youtube/v3',
+          params: {key: key},
+        });
     }
 
     async mostPopular() {
-        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResult=25&key=${this.key}`,
-            this.getRequestOptions
-        );
-        const result_1 = await response.json();
-        return result_1.items; //app.jsx에 함수인자를 setVideos라고 만들어 두엇지만 여기 함수인자를 result라고 만들어도 상관없음
+         const response = await this.youtube.get('videos', {
+             params: {
+              part: 'snippet',
+              chart: 'mostPopular',
+              maxResults: 25, 
+             }, 
+        });
+            return response.data.items;
     }
 
     async search(query) {
-        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=${this.key}`,
-            this.getRequestOptions
-        );
-        const result_1 = await response.json();
-        return result_1.items.map(item => ({ ...item, id: item.id.videoId })); //result.items 배열을 빙글 빙글 돌면서 각각의 배열 아이템들의 id값을 새로운 id로 덮어 씌워줌
+          const response = await this.youtube.get('search', {
+              params: {
+               part: 'snippet',
+               maxResults: 25,
+               type: 'video',
+               q: query,
+            },
+          });        
+        return response.data.items.map(item => ({ ...item, id: item.id.videoId })); //items.map 배열을 빙글 빙글 돌면서 각각의 배열 아이템들의 id값을 새로운 id로 덮어 씌워줌
      }
    
 }
